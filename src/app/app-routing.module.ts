c65acaffac2cc4a8e2bb44eee174c8af936c84f2
arshasaju <60867765+arshasaju@users.noreply.github.com>
2023-02-01 13:35:52 +0530

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'education',component:EducationComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'resume',component:ResumeComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'projects',component:ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
